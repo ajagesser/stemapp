2709de23905dfed77d4f2b657fce66dce72db447
Randy Tjin Asjoe <randy@milvum.com>
2017-02-11 09:10:45 +0100

    Updated: Login button navigates to PartyListActivity

--- app/src/main/java/com/milvum/stemapp/HomeActivity.java
+++ app/src/main/java/com/milvum/stemapp/HomeActivity.java
@@ -1,7 +1,10 @@
 package com.milvum.stemapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button loginButton = (Button) this.findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent i = new Intent(HomeActivity.this, PartyListActivity.class);
+                        startActivity(i);
+                    }
+                }
+        );
     }
 }
+
